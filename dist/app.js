ff0545e6363d328da59d6add1128278242e3d501
kevin Cotrina <kcotrinam92@gmail.com>
2021-02-05 23:21:42 -0500

    Refactor card

--- dist/app.js
+++ dist/app.js
@@ -43,33 +43,46 @@ class Weather {
   transformTemp (temp) {
     switch (this.measurement) {
       case 'fahrenheit':
-        return Math.round(temp - 273.15) * 9 / 5 + 32;
+        return `${Math.round(temp - 273.15) * 9 / 5 + 32} °F`;
       case 'celsius':
-        return Math.round(temp - 273.15);
+        return `${Math.round(temp - 273.15)} °C`;
       default:
-        return temp
+        return `${temp} °K`
     }
   }
 
   get populateContainers () {
     this.cityContainer.innerHTML = ''
-    console.log()
     const temp = this.transformTemp(this.value.main.temp)
     const mintemp = this.transformTemp(this.value.main.temp_min)
     const maxtemp = this.transformTemp(this.value.main.temp_max)
+    const feelsLike = this.transformTemp(this.value.main.feels_like)
     const card =`
                   <div class="card">
-                    <h1 class="card__city">${this.value.name}</h1>
-                    <h2 class="card__temperature">Temperature: ${temp} °C</h2>
-                    <p class="card__min-temperature">Temperature minimum: ${mintemp} °C</p>
-                    <img src= "http://openweathermap.org/img/w/${this.value.weather[0].icon}.png" alt="${this.value.weather[0].description}"/>
-                    <p class="card__max-temperature">Temperature maximum: ${maxtemp} °C</p>
+                    <div>
+                      <p>
+                        ${this.value.weather[0].description}
+                        <span>
+                          <img src= "http://openweathermap.org/img/w/${this.value.weather[0].icon}.png" alt="${this.value.weather[0].description}"/>
+                        </span>
+                      </p>
+                    </div>
+                    <h1 class="card__city">
+                      ${this.value.name}
+                      <span>${this.value.sys.country}</span>
+                    </h1>
+                    <h2 class="card__temperature">Temperature: ${temp}</h2>
+                    <p class="card__min-temperature">Temperature minimum: ${mintemp}</p>
+                    <p class="card__max-temperature">Temperature maximum: ${maxtemp}</p>
+                    <p class="card__feels">Feels like: ${feelsLike}</p>
+                    <p class="card__feels">Humidity: ${this.value.main.humidity}%</p>
                   </div>
                 `
     return card
   }
 
   render () {
+    console.log(this.value)
     this.cityContainer.insertAdjacentHTML('afterbegin', this.populateContainers)
   }
 }
