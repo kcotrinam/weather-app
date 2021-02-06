43e26425b3cd3b55e894df96210ceda6e2fa5124
kevin Cotrina <kcotrinam92@gmail.com>
2021-02-06 04:58:16 -0500

    Add readme file

--- src/modules/request.js
+++ src/modules/request.js
@@ -2,7 +2,7 @@
 const apiKey = '90389a28c75f30a2126f4ec7e1c08520';
 
 const getData = async (city) => {
-  const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`);
+  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`);
   const json = response.json();
   return json;
 };
